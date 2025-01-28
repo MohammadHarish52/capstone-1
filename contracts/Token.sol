10ac5eb7cf0821bd4cddba685c86fb1a3f82dce0
tachyonok <tachyonparticle123@gmail.com>
2025-01-28 14:33:49 +0530

    testing contracts

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -6,4 +6,15 @@ import "hardhat/console.sol";
 contract Token {
    string public name = "My Token";
    string public symbol = "TACH";
+   uint public decimals = 18;
+   uint public totalSupply = 1000000 * (10 ** decimals);
+
+   constructor(string memory _name , string memory _symbol , uint _decimals , uint _totalSupply) {
+       name = _name;
+       symbol = _symbol;
+       decimals = _decimals;
+       totalSupply = _totalSupply ;
+
+   }
+
 }
--- test/Token.js
+++ test/Token.js
@@ -1,6 +1,9 @@
 const { ethers } = require("hardhat");
 const { expect } = require("chai");
 
+const tokens = (n) => {
+  return ethers.parseUnits(n.toString(), "ether");
+};
 describe("Token", () => {
   let token;
 
@@ -8,16 +11,32 @@ describe("Token", () => {
     // Fetch the token contract factory
     const Token = await ethers.getContractFactory("Token");
     // Deploy the token
-    token = await Token.deploy();
+    token = await Token.deploy(
+      "Harish is amazing",
+      "HARISH",
+      18,
+      tokens(1000000)
+    );
     // Wait for deployment
     await token.waitForDeployment();
   });
+  describe("Deployment", () => {
+    const name = "Harish is amazing";
+    const symbol = "HARISH";
+    const decimals = 18;
+    it("Check if name is correct", async () => {
+      expect(await token.name()).to.equal(name);
+    });
 
-  it("Check if name is correct", async () => {
-    expect(await token.name()).to.equal("My Token");
-  });
-
-  it("Check if symbol is correct", async () => {
-    expect(await token.symbol()).to.equal("TACH");
+    it("Check if symbol is correct", async () => {
+      expect(await token.symbol()).to.equal(symbol);
+    });
+    it("Check if decimals is correct", async () => {
+      expect(await token.decimals()).to.equal(decimals);
+    });
+    it("Check if total supply is correct", async () => {
+      const value = tokens(1000000);
+      expect(await token.totalSupply()).to.equal(value);
+    });
   });
 });

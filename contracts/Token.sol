e8401595e03647084710a092e1b6054fa3bc183c
tachyonok <tachyonparticle123@gmail.com>
2025-01-30 12:42:37 +0530

    emittting the evnt transfer event

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,17 +4,34 @@ pragma solidity ^0.8.28;
 import "hardhat/console.sol";
 
 contract Token {
-   string public name = "My Token";
-   string public symbol = "TACH";
-   uint public decimals = 18;
-   uint public totalSupply = 1000000 * (10 ** decimals);
+    string public name = "My Token";
+    string public symbol = "TACH";
+    uint public decimals = 18;
+    uint public totalSupply = 1000000 * (10 ** decimals);
 
-   constructor(string memory _name , string memory _symbol , uint _decimals , uint _totalSupply) {
-       name = _name;
-       symbol = _symbol;
-       decimals = _decimals;
-       totalSupply = _totalSupply ;
+    //Track balances
+    mapping(address => uint) public balanceOf;
+    // Defining an event
+    event Transfer(address indexed from, address indexed to, uint value);
 
-   }
-
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint _decimals,
+        uint _totalSupply
+    ) {
+        name = _name;
+        symbol = _symbol;
+        decimals = _decimals;
+        totalSupply = _totalSupply;
+        balanceOf[msg.sender] = totalSupply;
+    }
+    function transfer(address _to, uint _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value);
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        // emitting an event
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
 }
--- test/Token.js
+++ test/Token.js
@@ -5,7 +5,7 @@ const tokens = (n) => {
   return ethers.parseUnits(n.toString(), "ether");
 };
 describe("Token", () => {
-  let token;
+  let token, owner, reciever;
 
   beforeEach(async () => {
     // Fetch the token contract factory
@@ -15,10 +15,12 @@ describe("Token", () => {
       "Harish is amazing",
       "HARISH",
       18,
-      tokens(1000000)
+      tokens(1000000),
     );
     // Wait for deployment
     await token.waitForDeployment();
+    // get the owner
+    [owner, reciever] = await ethers.getSigners();
   });
   describe("Deployment", () => {
     const name = "Harish is amazing";
@@ -37,6 +39,28 @@ describe("Token", () => {
     it("Check if total supply is correct", async () => {
       const value = tokens(1000000);
       expect(await token.totalSupply()).to.equal(value);
+    });
+    it("Assigning total supply to deployer", async () => {
+      expect(await token.balanceOf(owner.address)).to.equal(tokens(1000000));
+    });
+  });
+  describe("Sending tokens", () => {
+    let amount, transaction, receipt;
+    it("Transfer Token balance", async () => {
+      // transfer from owner to reciever
+      amount = tokens(100);
+      transaction = await token.connect(owner).transfer(reciever, amount);
+      receipt = await transaction.wait();
+
+      expect(await token.balanceOf(owner.address)).to.equal(tokens(999900));
+      expect(await token.balanceOf(reciever.address)).to.equal(tokens(100));
+    });
+    it("Emits a Transfer event", async () => {
+      const event = receipt.logs[0];
+      expect(event.fragment.name).to.equal("Transfer");
+      expect(event.args[0]).to.equal(owner.address);
+      expect(event.args[1]).to.equal(reciever.address);
+      expect(event.args[2]).to.equal(amount);
     });
   });
 });
